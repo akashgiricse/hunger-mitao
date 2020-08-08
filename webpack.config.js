bec388a5ba630308f6e1c83e3b69e7c8dc6d39d2
akashgiricse <akashgiricse@gmail.com>
2020-08-08 18:50:34 +0530

    Webpack configuration done

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: ["@babel/polyfill", "./src/js/index.js"],
   output: {
-    path: path.join(__dirname, "dist/js"),
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/main.js",
   },
   devServer: {
     contentBase: "./dist",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
 };
