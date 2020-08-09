8c8a4c8942e96ae8ab1d4e189cf014f802a982bb
akashgiricse <akashgiricse@gmail.com>
2020-08-09 14:38:24 +0530

    Add search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,42 @@
-import num from "./test";
+/**
+ * This module works as controller
+ * Mode and views are in models and views folder
+ */
 
-const x = 222;
+import Search from "./models/Search";
+import { elements } from "./views/base";
+import * as searchView from "./views/searchView";
 
-console.log(`num is ${num} and constant x is ${x}`);
+/**
+ * The state object below is Global state of the app
+ * It includes following states
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipe
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1. Get the query from view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2. New search object and save it to our state object
+    state.search = new Search(query);
+    // 3. Prepare UI for result
+    searchView.clearInput();
+    searchView.clearResults();
+    // 4. Search for recipe
+    await state.search.getResults();
+
+    // 5. Render result on UI
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
---
+++ src/js/views/base.js
@@ -0,0 +1,9 @@
+/**
+ * This file contains all the query selectors objects
+ */
+
+export const elements = {
+  searchForm: document.querySelector(".search"),
+  searchInput: document.querySelector(".search__field"),
+  searchResList: document.querySelector(".results__list"),
+};
